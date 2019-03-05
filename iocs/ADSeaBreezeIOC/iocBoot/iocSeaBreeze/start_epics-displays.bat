b4f027db45164744357d2216406c7ce235b25049
Mark Engbretson <engbretson@anl.gov>
2019-03-05 12:40:24 -0600

    Add medm adl files

--- iocs/ADSeaBreezeIOC/iocBoot/iocSeaBreeze/start_epics-displays.bat
+++ iocs/ADSeaBreezeIOC/iocBoot/iocSeaBreeze/start_epics-displays.bat
@@ -1,6 +1,4 @@
-set SUPPORT=C:\jhSrc\synApps_5_7\support
-set AREA_DETECTOR=%SUPPORT%\areaDetector_2_git
-set ASYN=%SUPPORT%\asyn-4-23
-set EPICS_DISPLAY_PATH=%AREA_DETECTOR%\ADPICam\PICamApp\op\adl;%AREA_DETECTOR%\ADCore\ADApp\op\adl;%ASYN%\opi\medm
+
+set EPICS_DISPLAY_PATH=Z:\epics\synApps_6_0\support\areaDetector-R3-4\ADSeaBreeze\SeaBreezeApp\op\adl;Z:\epics\synApps_6_0\support\areaDetector-R3-4\ADCore-R3-4\ADApp\op\adl
 echo %EPICS_DISPLAY_PATH%
-start medm -x -macro "P=13PICAM1:, R=cam1:" PICAMBase.adl
+start medm -x -macro "P=HIBEF:XFEL:, R=cam1:" ADSeaBreeze.adl
